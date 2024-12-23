68ed5cadde897d4b74e196d4c5d70bc720142688
Naoval Octa <160556665+NaovalOcta@users.noreply.github.com>
2024-12-23 12:18:00 +0700

    Delete TugasBesarUAP directory

--- TugasBesarUAP/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.tugasbesaruap {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.codelab.tugasbesaruap to javafx.fxml;
-    exports com.codelab.tugasbesaruap;
-}
